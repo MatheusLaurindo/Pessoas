233f85dd06bf84534bd7dcdd8edf18b68586b8c7
Matheus Figueredo <matheuslaurindo2017@gmail.com>
2025-05-12 17:51:23 -0300

    configuracoes da spa

--- Pessoas.Server/Program.cs
+++ Pessoas.Server/Program.cs
@@ -1,14 +1,18 @@
-using Pessoas.Server.Infra;
 using Pessoas.Server.Infra.Extensoes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Habilita arquivos estáticos do SPA
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "../pessoas.client/dist";
+});
 
 // Instalar serviços via padrão Installer
 builder.Services.InstallServicesFromAssembly(builder.Configuration);
@@ -17,6 +21,7 @@ var app = builder.Build();
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+app.UseSpaStaticFiles(); // Agora vai funcionar sem erro
 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
@@ -26,21 +31,21 @@ app.UseSwaggerUI(options =>
     options.DocumentTitle = "Pessoas.API Docs";
 });
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-    Seeding.SeedPessoas(db);
-    Seeding.SeedUsuarios(db);
-}
-
 app.UseHttpsRedirection();
-app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
+app.UseRouting();
 
 app.UseCors("AllowSpecificOrigin");
 app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
+
+// Fallback para SPA
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "../pessoas.client"; // usado apenas no dev
+});
+
+app.MapFallbackToFile("/index.html");
+
 app.Run();
